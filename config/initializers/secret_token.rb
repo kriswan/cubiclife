abd37dc5161558be9073318bd33a33e036a82bef
shunfei.wan <shunfei.wan@qunar.com>
2014-10-14 11:55:35 +0800

    connect local to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cubiclife::Application.config.secret_key_base = '89ab57497f397d8dfc8150dadeb019fae96deadc4a1425240bde2288926c448972af5a8eda267dae357ff373eb35bf81a7afc77a58b77192b0157a6c58975763'
